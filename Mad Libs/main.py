3ad72fb6e8e8c810fba3cb17b3995a13858c5a0a
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-12-12 22:52:35 +0600

    Added main.py file.

---
+++ Mad Libs/main.py
@@ -0,0 +1,5 @@
+word1 = input("Enter first word: ")
+word2 = input("Enter second word: ")
+word3 = input("Enter third word: ")
+
+print(f"There once was a man named {word1}.\nHe was {word2} years old.\nHe liked name {word3}, but did not liked his age.")
